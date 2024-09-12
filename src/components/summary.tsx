9cda844c9f3aaf94cff881934baf8e4cfab6682d
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2024-09-12 01:39:48 -0300

    fix: prevent crashing by giving an option for when no goal was completed

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -71,36 +71,44 @@ export function Summary() {
       <div className="flex flex-col gap-6">
         <h2 className="text-xl font-medium">Sua semana</h2>
 
-        {Object.entries(data.goalsPerDay).map(([date, goals]) => {
-          const weekDay = dayjs(date).format('dddd')
-          const formattedDay = dayjs(date).format('D [de] MMMM')
+        {data.goalsPerDay ? (
+          Object.entries(data.goalsPerDay).map(([date, goals]) => {
+            const weekDay = dayjs(date).format('dddd')
+            const formattedDay = dayjs(date).format('D [de] MMMM')
 
-          return (
-            <div key={date} className="flex flex-col gap-4">
-              <h3 className="font-medium">
-                <span className="capitalize">{weekDay}</span>{' '}
-                <span className="text-xs text-zinc-400">({formattedDay})</span>
-              </h3>
+            return (
+              <div key={date} className="flex flex-col gap-4">
+                <h3 className="font-medium">
+                  <span className="capitalize">{weekDay}</span>{' '}
+                  <span className="text-xs text-zinc-400">
+                    ({formattedDay})
+                  </span>
+                </h3>
 
-              <ul className="flex flex-col gap-3">
-                {goals.map(goal => {
-                  const time = dayjs(goal.completedAt).format('HH:mm[h]')
+                <ul className="flex flex-col gap-3">
+                  {goals.map(goal => {
+                    const time = dayjs(goal.completedAt).format('HH:mm[h]')
 
-                  return (
-                    <li key={goal.id} className="flex items-center gap-2">
-                      <CheckCircle2 className="size-4 text-pink-500" />
-                      <span className="text-sm text-zinc-400">
-                        Você completou “
-                        <span className="text-zinc-100">{goal.title}</span>” às{' '}
-                        <span className="text-zinc-100">{time}</span>
-                      </span>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-          )
-        })}
+                    return (
+                      <li key={goal.id} className="flex items-center gap-2">
+                        <CheckCircle2 className="size-4 text-pink-500" />
+                        <span className="text-sm text-zinc-400">
+                          Você completou “
+                          <span className="text-zinc-100">{goal.title}</span>”
+                          às <span className="text-zinc-100">{time}</span>
+                        </span>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            )
+          })
+        ) : (
+          <span className="text-sm text-zinc-400">
+            Você ainda não completou nenhuma meta essa semana.
+          </span>
+        )}
       </div>
     </div>
   )
